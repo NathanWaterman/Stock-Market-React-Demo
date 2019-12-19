7c2ff3e749f02e286d0e10f10760454d3fac462c
nwaterman <nwaterman@nbme.org>
2019-12-19 13:44:50 -0500

    merge master into branch

--- src/components/App.js
+++ src/components/App.js
@@ -6,152 +6,6 @@ import StockNews from "./news/StockNews";
 import StockChart from "./chart/StockChart";
 import ErrorUI from "./Error";
 
-<<<<<<< HEAD
-class App extends Component {
-	state = {
-		quoteData: "",
-		newsData: [],
-		chartData: [],
-		stockImg: "",
-		searchTerm: "ba",
-		isLoading: true,
-		isError: false
-	};
-
-	//enter stock symbol and submit query
-	onTermSubmit = async term => {
-		//on submit match symbol or companyName
-		//EX: "symbol":"FB","companyName":"Facebook"
-		await setSearchTerm(term);
-		loadStockData();
-		console.log(term);
-	};
-
-	removeErr = updateErr => {
-		this.setState({ isError: updateErr });
-	}
-
-	//load stock data from API
-	loadStockData = () => {
-		//load stock info
-		stockInfo(this.state.searchTerm, window.ACCESS_TOKEN)
-			.then(res => {
-				this.setState({
-					quoteData: res.data,
-					isError: false
-				});
-			})
-			.catch(err => {
-				this.setState({ isError: true });
-				return err;
-			});
-		//load stock logo img
-		stockLogo(this.state.searchTerm, window.ACCESS_TOKEN)
-			.then(res => {
-				this.setState({
-					stockImg: res.data.url
-				});
-			})
-			.catch(err => err);
-		//load stock news
-		stockNews(this.state.searchTerm, window.ACCESS_TOKEN)
-			.then(res => {
-				this.setState({ 
-					newsData: res.data,
-					// isLoading: false
-				 });
-			})
-			.catch(err => err);
-		//load stock chart range data
-		stockChart(this.state.searchTerm, window.ACCESS_TOKEN)
-			.then(res => {
-				this.setState({ 
-					chartData: res.data,
-					isLoading: false
-				 });
-			})
-			.catch(err => err);
-	};
-
-	componentDidMount() {
-		this.loadStockData();
-	}
-
-	render() {
-
-		if (this.state.isLoading) {
-			return (
-				<div>
-					<div className="ui segment loading-view">
-						<div className="ui active dimmer">
-							<div className="ui massive text loader">Loading</div>
-						</div>
-						<p></p>
-						<p></p>
-						<p></p>
-					</div>
-				</div>
-			);
-		}
-		else if (!this.state.isLoading) {
-			return (
-				<div className="ui main-view">
-					<div className="ui stackable one column grid">
-						<div className="row">
-							<div className="column">
-								{this.state.isError ? <ErrorUI removeErr={this.removeErr} /> : ''}
-								<div className="sixteen wide column">
-									<SearchBar searchTerm={this.onTermSubmit} />
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="ui stackable two column grid">
-						<div className="row">
-							<div className="column">
-								<div className="row">
-									<div className="column">
-										<StockQuote
-											quoteData={this.state.quoteData}
-											stockImg={this.state.stockImg}
-										/>
-									</div>
-									<div className="column">
-										<div className="news-container ui fluid card">
-											{this.state.newsData.length !== 0 ? <StockNews data={this.state.newsData} title={this.state.quoteData.companyName} /> : <h3 className="no-news">There is no Available News</h3>}
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="column">
-								<div className="row">
-									<div className="column">
-										<div className="chart-container ui fluid card">
-											{this.state.chartData.length !== 0 ? <StockChart data={this.state.chartData} title={this.state.quoteData.companyName}/> : <h3 className="no-chart">There is no Available Stock Chart</h3>}
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			);
-		}
-	}
-}
-export default App;
-
-// const App = () => {
-
-// 	const [newsData, setNewsData] = useState([]);
-// 	const [chartData, setChartData] = useState([]);
-// 	const [searchTerm, setSearchTerm] = useState("ba");
-// 	const [stockImg, setStockImg] = useState("");
-// 	const [quoteData, setQuoteData] = useState("");
-// 	const [isLoading, setLoading] = useState(true);
-// 	const [isError, setError] = useState(false);
-	
-=======
 // class App extends Component {
 // 	state = {
 // 		quoteData: "",
@@ -177,7 +31,6 @@ export default App;
 // 		this.setState({ isError: updateErr });
 // 	}
 
->>>>>>> master
 // 	//load stock data from API
 // 	loadStockData = () => {
 // 		//load stock info
@@ -220,31 +73,11 @@ export default App;
 // 			.catch(err => err);
 // 	};
 
-<<<<<<< HEAD
-// 	useEffect(() => {
-// 		loadStockData();
-// 		console.log(isError);
-//     }, [searchTerm]);
-
-// 	//enter stock symbol and submit query
-// 	const onTermSubmit = async term => {
-// 		//on submit match symbol or companyName
-// 		//EX: "symbol":"FB","companyName":"Facebook"
-// 		await setSearchTerm(term);
-// 		loadStockData();
-// 	};
-
-// 	const removeErr = updateErr => {
-// 		setError(updateErr);
-// 	}
-
-=======
 // 	componentDidMount() {
 // 		this.loadStockData();
 // 	}
 
 // 	render() {
->>>>>>> master
 
 // 		if (this.state.isLoading) {
 // 			return (
